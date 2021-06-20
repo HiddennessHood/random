b9859b0aa6009ca86036e2ba5962ccd0df8a5cb6
HiddennessHood <85771637+HiddennessHood@users.noreply.github.com>
2021-06-20 05:26:32 +0300

    Create twitchplayer.js

---
+++ twitchplayer.js
@@ -0,0 +1,8 @@
+  var options = {
+    width: 340,
+    height: 192,
+    channel: "honeymad",
+    parent: ["hiddennesshood.github.io"]    
+  };
+  var player = new Twitch.Player("streamerplayer", options);
+  player.setVolume(1);
