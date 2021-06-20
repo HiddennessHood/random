a0c7efaf21e39a62498f967e3cc9de88a40b3f30
HiddennessHood <85771637+HiddennessHood@users.noreply.github.com>
2021-06-20 05:35:26 +0300

    Delete twitchplayer.js

--- twitchplayer.js
+++
@@ -1,8 +0,0 @@
-var options = {
-    width: 340,
-    height: 192,
-    channel: "honeymad",
-    parent: ["hiddennesshood.github.io"]    
-  };
-  var player = new Twitch.Player("streamerplayer", options);
-  player.setVolume(1);
