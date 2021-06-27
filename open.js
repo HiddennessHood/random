6ff50a1db794dac52d75031d864e38a95f4c8b0f
HiddennessHood <85771637+HiddennessHood@users.noreply.github.com>
2021-06-27 22:28:15 +0300

    Update open.js

--- open.js
+++ open.js
@@ -11,3 +11,16 @@ function openChat(evt, chatName) {
     document.getElementById(chatName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+function openv(evt, chatName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabchat");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("vbutton");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(chatName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
